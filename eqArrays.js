aaf29409db29c15c9042d16e690630e0e30a8471
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-07-23 11:57:01 -0400

    write function for comparing arrays in lotide

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+const eqArrays = (a, b) => {
+
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length !== b.length) return false;
+
+  for (let i = 0; i < a.length; i++) {
+    if (a[i] != b[i])
+      return false;
+
+  }
+  return true;
+}
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assertEqual(eqArrays([1, 2, 3], [2, 2, 3]), false);
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true) // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false) // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false) // => false
