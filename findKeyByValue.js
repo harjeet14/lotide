ffaf51110c2bda12583d8ab2f63c615f8aab8e7e
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-03 16:06:14 -0400

    getting key and value

---
+++ findKeyByValue.js
@@ -0,0 +1,28 @@
+const assertEqual = (actual, expected) => {
+  if (actual === expected) {
+    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+  } else {
+    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+  }
+};
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+const findKeyByValue = (inputObject, values) => {
+  const objectKeys = Object.keys(inputObject);
+  for (let keys of objectKeys) {
+
+    if (inputObject[keys] === values) {
+      return keys;
+    }
+  }
+};
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
