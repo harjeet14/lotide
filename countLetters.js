25022fb5d570c1e3211682e5e417bea9f1865be2
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-10 15:04:00 -0400

    refactor countLetters

--- countLetters.js
+++ countLetters.js
@@ -1,12 +1,12 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+// const assertEqual = (actual, expected) => {
+//   if (actual === expected) {
+//     console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
+//   } else {
+//     console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
+//   }
+// };
+// assertEqual("Lighthouse Labs", "Bootcamp");
+// assertEqual(1, 1);
 
 const countLetters = (str) => {
   let result = {};
@@ -14,15 +14,12 @@ const countLetters = (str) => {
   for (let i = 0; i < str.length; i++) {
     if (str[i] !== " ") {
       if (result[str[i]]) {
-        result[str[i]] += 1
+        result[str[i]] += 1;
       } else {
-        result[str[i]] = 1
+        result[str[i]] = 1;
       }
     }
   }
   return result;
-}
-console.log(countLetters('light house labs'));
-
-//assertEqual(countLetters('LightHouseLabs'), );
-//assertEqual(countLetters('LHL', 'H'), 1);
+};
+module.exports = countLetters;
