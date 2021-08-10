7f54873135e499e3c47b8b786eae0d15fc5aba3d
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-10 14:07:07 -0400

    mocha and chai on middle and tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -2,9 +2,6 @@ const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-// assertEqual(result.length, 2);
-// assertEqual(result[0], "Lighthouse");
-// assertEqual(result[1], "Labs");
 
 describe("#tail", () => {
 
