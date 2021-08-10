2103b7960755e3ee10cc8d3dd71b3b62e6ba89e1
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-10 14:04:45 -0400

    mocha and chai tests for head, middle and tail

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,15 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2);
-assertEqual(result[0], "Lighthouse");
-assertEqual(result[1], "Labs");
+// assertEqual(result.length, 2);
+// assertEqual(result[0], "Lighthouse");
+// assertEqual(result[1], "Labs");
+
+describe("#tail", () => {
+
+  it("returns  Labs for [Hello, Lighthouse, Labs]", () => {
+    assert.deepEqual((result), ["Lighthouse", "Labs"]);
+  })
+
+});
