c4b6ca4f60cc3e053f87557af5612fa99f3b6f12
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-10 16:23:49 -0400

    export one object with all functions

---
+++ test.js
@@ -0,0 +1,26 @@
+// for odd
+// let array = [1, 2, 3, 4, 5];
+// let midIndex = Math.floor(array.length / 2);
+// //console.log(result)
+// let value = array[midIndex];
+// console.log(value);  
+
+
+// for even 
+
+// let arr = []
+
+// let array2 = [10, 20, 30, 40, 50, 60]
+// let midIndex2 = Math.floor((array2.length) / 2)
+// arr.push(array2[midIndex2]);
+// let value2 = array2[midIndex2 - 1];
+// arr.push(value2)
+// console.log(arr)
+let array = []
+//let res = [];
+if (array.length <= 2) {
+  return array;
+}else{
+  return 
+}
+
