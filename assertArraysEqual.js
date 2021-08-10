3e56f8e9743e16ce842205dbedafbf971a6bf807
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-10 10:35:05 -0400

    refactor assertArraysEqual with require eqarray and export assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,3 +1,4 @@
+const eqArrays = require("./eqArrays");
 const assertArraysEqual = (actual, expected) => {
 
   if (eqArrays(actual, expected)) {
@@ -5,18 +6,7 @@ const assertArraysEqual = (actual, expected) => {
   } else {
     console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
   }
-}
-const eqArrays = (a, b) => {
+};
 
-  if (a === b) return true;
-  if (a.length !== b.length) return false;
 
-  for (let i = 0; i < a.length; i++) {
-    if (a[i] != b[i])
-      return false;
-
-  }
-  return true;
-}
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [2, 2, 3]);
+module.exports = assertArraysEqual;
