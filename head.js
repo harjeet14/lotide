c9061b9f39f3ed14ea6f474fa0aeca2ca4974792
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-09 21:11:02 -0400

    refactor head using module export

--- head.js
+++ head.js
@@ -1,16 +1,4 @@
-const assertEqual = (actual, expected) => {
-  if (actual === expected) {
-    console.log(`✅ Assertion Passed: [${actual}] === [${expected}]`);
-  } else {
-    console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
-  }
-};
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-
 const head = (array) => {
   return array[0];
-}
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]));
+};
+module.exports = head;
