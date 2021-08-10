a4ba561e0c9bddfeaa1d851ac786a89b11b86c99
Harjeet Kaur <harjeetkaur.dhanjal@gmail.com>
2021-08-09 21:07:04 -0400

    refactor code with module export

--- assertEqual.js
+++ assertEqual.js
@@ -5,5 +5,4 @@ const assertEqual = (actual, expected) => {
     console.log(`❌ Assertion Failed: [${actual}] !== [${expected}]`);
   }
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
+module.exports = assertEqual;
